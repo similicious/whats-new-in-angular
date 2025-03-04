252e324837486e35a08b4d630ff0a37ec207098c
similicious <mail@similicious.de>
2025-03-04 11:16:01 +0100

    restructure app

--- src/app/topics/part-01-deferred-views/components/deferred-views/part-01-deferred-views.component.ts
+++ src/app/topics/part-01-deferred-views/components/deferred-views/part-01-deferred-views.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { Deferred01Component } from './deferred-01/deferred-01.component';
-import { Deferred02Component } from './deferred-02/deferred-02.component';
-import { Deferred03Component } from './deferred-03/deferred-03.component';
+import { Deferred01Component } from '../deferred-01/deferred-01.component';
+import { Deferred02Component } from '../deferred-02/deferred-02.component';
+import { Deferred03Component } from '../deferred-03/deferred-03.component';
 
 @Component({
   selector: 'app-part-01-deferred-views',
